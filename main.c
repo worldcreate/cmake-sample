5eadf3d3f15e7ad3414e0ab3079700299b12ac66
worldcreate <furatsuki_fiskm@yahoo.co.jp>
2017-11-29 01:43:26 +0900

    インデントをタブに一括変換

--- main.c
+++ main.c
@@ -11,55 +11,55 @@
 
 int main(int argc, char** argv) {
 
-    int servSock; //server socket descripter
-    int clitSock; //client socket descripter
-    struct sockaddr_in servSockAddr; //server internet socket address
-    struct sockaddr_in clitSockAddr; //client internet socket address
-    unsigned short servPort; //server port number
-    unsigned int clitLen; // client internet socket address length
+	int servSock; //server socket descripter
+	int clitSock; //client socket descripter
+	struct sockaddr_in servSockAddr; //server internet socket address
+	struct sockaddr_in clitSockAddr; //client internet socket address
+	unsigned short servPort; //server port number
+	unsigned int clitLen; // client internet socket address length
 	int yes = 1;
 
-    if ( argc != 2) {
-        fprintf(stderr, "argument count mismatch error.\n");
-        exit(EXIT_FAILURE);
-    }
+	if ( argc != 2) {
+		fprintf(stderr, "argument count mismatch error.\n");
+		exit(EXIT_FAILURE);
+	}
 
-    if ((servPort = (unsigned short) atoi(argv[1])) == 0) {
-        fprintf(stderr, "invalid port number.\n");
-        exit(EXIT_FAILURE);
-    }
+	if ((servPort = (unsigned short) atoi(argv[1])) == 0) {
+		fprintf(stderr, "invalid port number.\n");
+		exit(EXIT_FAILURE);
+	}
 
-    if ((servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0 ){
-        perror("socket() failed.");
-        exit(EXIT_FAILURE);
-    }
+	if ((servSock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0 ){
+		perror("socket() failed.");
+		exit(EXIT_FAILURE);
+	}
 
-    memset(&servSockAddr, 0, sizeof(servSockAddr));
-    servSockAddr.sin_family      = AF_INET;
-    servSockAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servSockAddr.sin_port        = htons(servPort);
+	memset(&servSockAddr, 0, sizeof(servSockAddr));
+	servSockAddr.sin_family	  = AF_INET;
+	servSockAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servSockAddr.sin_port		= htons(servPort);
 
 	setsockopt(servSock, SOL_SOCKET, SO_REUSEADDR, (const char *)&yes, sizeof(yes));
 
-    if (bind(servSock, (struct sockaddr *) &servSockAddr, sizeof(servSockAddr) ) < 0 ) {
-        perror("bind() failed.");
-        exit(EXIT_FAILURE);
-    }
+	if (bind(servSock, (struct sockaddr *) &servSockAddr, sizeof(servSockAddr) ) < 0 ) {
+		perror("bind() failed.");
+		exit(EXIT_FAILURE);
+	}
 
-    if (listen(servSock, QUEUELIMIT) < 0) {
-        perror("listen() failed.");
-        exit(EXIT_FAILURE);
-    }
+	if (listen(servSock, QUEUELIMIT) < 0) {
+		perror("listen() failed.");
+		exit(EXIT_FAILURE);
+	}
 
 	LOG("test");
 
-    while(1) {
+	while(1) {
 		char buff[1024];
-        clitLen = sizeof(clitSockAddr);
-        if ((clitSock = accept(servSock, (struct sockaddr *) &clitSockAddr, &clitLen)) < 0) {
-            perror("accept() failed.");
-            exit(EXIT_FAILURE);
-        }
+		clitLen = sizeof(clitSockAddr);
+		if ((clitSock = accept(servSock, (struct sockaddr *) &clitSockAddr, &clitLen)) < 0) {
+			perror("accept() failed.");
+			exit(EXIT_FAILURE);
+		}
 
 		int size = 0;
 		while ((size = read(clitSock , buff, 32)) > 0) {
@@ -71,12 +71,12 @@ int main(int argc, char** argv) {
 			}
 			printf("\n");
 		}
-		
-        printf("\n\nconnected from %s.\n", inet_ntoa(clitSockAddr.sin_addr));
-      close(clitSock);
-    }
 
-    return EXIT_SUCCESS;
+		printf("\n\nconnected from %s.\n", inet_ntoa(clitSockAddr.sin_addr));
+	  close(clitSock);
+	}
+
+	return EXIT_SUCCESS;
 
 	return 0;
 }
